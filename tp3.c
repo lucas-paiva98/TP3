c6346ba1e03e5f33c08504d1d1c3b0d721a0f0a3
Lucas Paiva <paiva.lucas1211@gmail.com>
2019-06-17 23:32:15 -0300

    Add linked list and graph strutures

--- tp3.c
+++ tp3.c
@@ -1,29 +1,148 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "utils.h"
+
+// A structure to represent an adjacency list node 
+struct AdjListNode { 
+  int value; 
+  struct AdjListNode* next; 
+}; 
+  
+// A structure to represent an adjacency list 
+struct AdjList { 
+  struct AdjListNode *head;  
+}; 
+
+struct Graph { 
+  int vertexs; 
+  struct AdjList* array; 
+};
+
+// A utility function to create a new adjacency list node 
+struct AdjListNode* newAdjListNode(int value) { 
+  struct AdjListNode* newNode = (struct AdjListNode*) malloc(sizeof(struct AdjListNode)); 
+  newNode->value = value; 
+  newNode->next = NULL; 
+  return newNode; 
+} 
+
+// A utility function that creates a graph of V vertices 
+struct Graph* createGraph(int vertexQuantity) { 
+    struct Graph* graph =  
+        (struct Graph*) malloc(sizeof(struct Graph)); 
+    graph->vertexs = vertexQuantity; 
+  
+    // Create an array of adjacency lists.  Size of  
+    // array will be vertexQuantity 
+    graph->array =  
+      (struct AdjList*) malloc(vertexQuantity * sizeof(struct AdjList)); 
+  
+    // Initialize each adjacency list as empty by  
+    // making head as NULL 
+    int i; 
+    for (i = 0; i < vertexQuantity; ++i) 
+        graph->array[i].head = NULL; 
+  
+    return graph; 
+} 
+  
+// Adds an edge to an undirected graph 
+void addEdge(struct Graph* graph, int src, int dest) { 
+    // Add an edge from src to dest.  A new node is  
+    // added to the adjacency list of src.  The node 
+    // is added at the begining 
+    struct AdjListNode* newNode = newAdjListNode(dest); 
+    newNode->next = graph->array[src].head; 
+    graph->array[src].head = newNode; 
+  
+    // Since graph is undirected, add an edge from 
+    // dest to src also 
+    newNode = newAdjListNode(src); 
+    newNode->next = graph->array[dest].head; 
+    graph->array[dest].head = newNode; 
+} 
+
+// Remove those strutures to use only the ones needed 
+// and to make east to understand the constrution 
+void printGraph(struct Graph* graph) { 
+    int v; 
+    for (v = 0; v < graph->vertexs; ++v) 
+    { 
+        struct AdjListNode* pCrawl = graph->array[v].head; 
+        printf("\n Adjacency list of vertex %d\n head ", v); 
+        while (pCrawl) 
+        { 
+            printf("-> %d", pCrawl->value); 
+            pCrawl = pCrawl->next; 
+        } 
+        printf("\n"); 
+    } 
+} 
+
+// The function to print vertex cover 
+void printVertexCover(struct Graph graph, int vertexQuantity) { 
+    // Initialize all vertices as not visited. 
+    int visited[vertexQuantity]; 
+    for (int i=0; i < vertexQuantity; i++) 
+        visited[i] = 0; 
+  
+    list<int>::iterator i; 
+  
+    // Consider all edges one by one 
+    for (int u=0; u<V; u++) 
+    { 
+        // An edge is only picked when both visited[u] and visited[v] 
+        // are false 
+        if (visited[u] == false) 
+        { 
+            // Go through all adjacents of u and pick the first not 
+            // yet visited vertex (We are basically picking an edge 
+            // (u, v) from remaining edges. 
+            for (i= adj[u].begin(); i != adj[u].end(); ++i) 
+            { 
+                int v = *i; 
+                if (visited[v] == false) 
+                { 
+                     // Add the vertices (u, v) to the result set. 
+                     // We make the vertex u and v visited so that 
+                     // all edges from/to them would be ignored 
+                     visited[v] = true; 
+                     visited[u]  = true; 
+                     break; 
+                } 
+            } 
+        } 
+    } 
+  
+    // Print the vertex cover 
+    for (int i=0; i<V; i++) 
+        if (visited[i]) 
+          cout << i << " "; 
+}
 
 int main(int argc, char const *argv[]) {
 	FILE *filePaths;
 	int vertexQuantity = 0;
 	int edgeQuantity = 0;
+  int auxU = 0;
+  int auxV = 0;
 
-	filePaths = fopen("trilhas1.txt", "r");
+	filePaths = fopen("trilha1.txt", "r");
 
 	if (filePaths == NULL) {
-    printf("Error! Cant't open the file.\n");
-  	exit(1);
+		printf("Error! Cant't open the file.\n");
+		exit(1);
 	}
 
 	fscanf(filePaths, "%d %d", &vertexQuantity, &edgeQuantity);
 
-	citiesMatrix = allocateMatrix(edgeQuantity, 2);
+  struct Graph* graph = createGraph(vertexQuantity); 
 
 	for (int i = 0; i < edgeQuantity; i++) {
-		for (int j = 0; j < 2; j++) {
-			fscanf(filePaths, "%lf", &citiesAux);
-			citiesMatrix[i][j] = citiesAux;
-		}
+    fscanf(filePaths, "%d %d", &auxU, &auxV);
+    addEdge(graph, auxU, auxV);
 	}
+
+  printGraph(graph); 
 
 	return 0;
 }
